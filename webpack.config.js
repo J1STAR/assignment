d8c0d46fa846e98d64eb7ae00811a6350ff4d1f5
qudgus21 <qudgus54321@naver.com>
2021-12-30 14:18:47 +0900

    set: webpack 설정

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  mode: 'development',
+
+  devtool: 'source-map',
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+
+    extensions: ['.jsx', '.js', '.tsx', '.ts'],
+  },
+
+  entry: {
+    app: './src/index.tsx',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new Dotenv({
+      path: path.resolve(__dirname, './.env'),
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './dist'),
+  },
+
+  devServer: {
+    historyApiFallback: true,
+  },
+};
