b22244ff3e816ed8a724009c6c1635214edd9e83
qudgus21 <qudgus54321@naver.com>
2021-12-31 16:27:55 +0900

    feat: api 요청 함수 생성

---
+++ src/util/api.ts
@@ -0,0 +1,16 @@
+const END_POINT = 'https://frontend-assignments.s3.ap-northeast-2.amazonaws.com/job_postings.json';
+
+export const request = async (url = '', options = {}) => {
+  try {
+    const fullUrl = `${END_POINT}${url}`;
+    const response = await fetch(fullUrl, options);
+    if (response.ok) {
+      const json = await response.json();
+      return json;
+    }
+    throw new Error('request fail!');
+  } catch (e) {
+    alert(e.message);
+    return [];
+  }
+};
