8669d181f8acfaa9b621d92086fcdbbb30d9c018
qudgus21 <qudgus54321@naver.com>
2021-12-31 21:15:47 +0900

    feat: 버튼 클릭시 모달 적용

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,6 +1,7 @@
 import { FC, useState, useCallback, useEffect } from 'react';
 import Header from '@/components/Header';
 import Calendar from '@/components/Calendar';
+import Modal from '@/components/Modal';
 import { request } from '@/util/api';
 import { JopData } from '@/util/types';
 
@@ -10,9 +11,11 @@ const Home: FC = () => {
   // const [year, setYear] = useState<number>(new Date().getFullYear());
   // const [month, setMonth] = useState<number>(new Date().getMonth() + 1);
   const [jobData, setJopData] = useState<JopData[]>([]);
+  const [isModalOpen, SetIsModalOpen] = useState<boolean>(false);
+  const [selectedJob, setSelectedJob] = useState<JopData>(null);
 
   /**
-   * 이전 버튼 클릭
+   * 헤더의 이전 버튼 클릭
    */
   const beforeMonth = useCallback(() => {
     if (month === 1) {
@@ -24,7 +27,7 @@ const Home: FC = () => {
   }, [year, month]);
 
   /**
-   * 이후 버튼 클릭
+   * 헤더의 이후 버튼 클릭
    */
   const afterMonth = useCallback(() => {
     if (month === 12) {
@@ -43,6 +46,27 @@ const Home: FC = () => {
     setJopData(data);
   };
 
+  /**
+   * 모달 숨김
+   */
+  const removeModal = () => {
+    SetIsModalOpen(false);
+  };
+
+  /**
+   * 채용공고 클릭
+   */
+  const handleJobClick = useCallback(
+    (e) => {
+      const $li = e.target.closest('li');
+      const { id } = $li.dataset;
+      const job = jobData.find((job) => job.id === Number(id)) as JopData;
+      setSelectedJob(job);
+      SetIsModalOpen(true);
+    },
+    [jobData],
+  );
+
   useEffect(() => {
     getInformation();
   }, []);
@@ -50,7 +74,8 @@ const Home: FC = () => {
   return (
     <>
       <Header year={year} month={month} beforeMonth={beforeMonth} afterMonth={afterMonth} />
-      <Calendar year={year} month={month} jobData={jobData} />
+      <Calendar year={year} month={month} jobData={jobData} handleJobClick={handleJobClick} />
+      <Modal job={selectedJob} removeModal={removeModal} visible={isModalOpen} />
     </>
   );
 };
